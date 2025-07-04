1759a3c3518f84cb9436a30b965dfbffe679a92c
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-07-04 11:47:53 -0400

    scaffold

---
+++ mariachi/repl.py
@@ -0,0 +1,15 @@
+"""REPL for the Mariachi toy language."""
+
+def repl():
+    print("Saludos desde el Mariachi REPL!")
+    print("Type 'exit()' to quit.\n")
+    while True:
+        code = input("mariachi> ")
+
+        if code == "exit()":
+            break
+        elif not code:
+            continue
+
+if __name__ == "__main__":
+    repl()
