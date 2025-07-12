67278fb35a887f2c8bb487fca894f09ef160523b
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-07-11 23:12:46 -0400

    that was miserable holy fuck, be careful what you copy and paste

--- mariachi/interpreter.py
+++ mariachi/interpreter.py
@@ -371,17 +371,17 @@ class BaseFunction(Value):
 
         if len(args) > len(arg_names):
             return res.failure(EjecucionError(
-				self.pos_start, self.pos_end,
-				f"{len(args) - len(arg_names)} demasiados argumentos pasados a '{self.name}'",
-				self.context
-			))
+                self.pos_start, self.pos_end,
+                f"{len(args) - len(arg_names)} demasiados argumentos pasados a '{self.name}'",
+                self.context
+            ))
 
         if len(args) < len(arg_names):
             return res.failure(EjecucionError(
-				self.pos_start, self.pos_end,
-				f"{len(arg_names) - len(args)} no suficiente argumentos pasados a '{self.name}'",
-				self.context
-			))
+                self.pos_start, self.pos_end,
+                f"{len(arg_names) - len(args)} no suficiente argumentos pasados a '{self.name}'",
+                self.context
+            ))
         return res.success(None)
     
     def populate_args(self, arg_names, args, exec_ctx):
@@ -399,10 +399,10 @@ class BaseFunction(Value):
         return res.success(None)
 
 class Function(BaseFunction):
-	def __init__(self, name, body_node, arg_names):
-		super().__init__(name)
-		self.body_node = body_node
-		self.arg_names = arg_names
+    def __init__(self, name, body_node, arg_names):
+        super().__init__(name)
+        self.body_node = body_node
+        self.arg_names = arg_names
 
     def execute(self, args):
         res = RTResult()
@@ -419,13 +419,13 @@ class Function(BaseFunction):
         return None
 
     def copy(self):
-		copy = Function(self.name, self.body_node, self.arg_names)
-		copy.set_context(self.context)
-		copy.set_position(self.pos_start, self.pos_end)
-		return copy
+        copy = Function(self.name, self.body_node, self.arg_names)
+        copy.set_context(self.context)
+        copy.set_position(self.pos_start, self.pos_end)
+        return copy
 
-	def __repr__(self):
-		return f"<funcion {self.name}>"
+    def __repr__(self):
+        return f"<funcion {self.name}>"
 
 class BuiltInFunction(BaseFunction):
     def __init__(self, name):
@@ -455,7 +455,7 @@ class BuiltInFunction(BaseFunction):
             return f"<built-in function {self.name}>"
 
         def execute_print(self, exec_ctx):
-            print(str(exec_ctx.symbol_table.get('value'))
+            print(str(exec_ctx.symbol_table.get('value')))
             return RTResult().success(Number.null)
         execute_print.arg_names = ['value']
 
@@ -478,110 +478,6 @@ class BuiltInFunction(BaseFunction):
                     print(f"{text} debe de ser un numero")
             return RTResult().success(Number(number))
         execute_input_int.arg_names = []
-
-        def execute_clear(self, exec_ctx):
-            os.system('cls' if os.name == 'nt' else 'cls')
-            return RTResult()success(Number.null)
-        execute_clear.arg_names = []
-
-        def execute_is_number(self, exec_ctx):
-            is_number = isinstance(exec_ctx.symbol_table.get("value"), Number)
-            return RTResult().success(Number.true if is_number else Number.false)
-        execute_is_number.arg_names = ["value"]
-
-        def execute_is_string(self, exec_ctx):
-            is_number = isinstance(exec_ctx.symbol_table.get("value"), String)
-            return RTResult().success(Number.true if is_number else Number.false)
-        execute_is_string.arg_names = ["value"]
-
-        def execute_is_list(self, exec_ctx):
-            is_number = isinstance(exec_ctx.symbol_table.get("value"), List)
-            return RTResult().success(Number.true if is_number else Number.false)
-        execute_is_list.arg_names = ["value"]
-
-        def execute_is_function(self, exec_ctx):
-            is_number = isinstance(exec_ctx.symbol_table.get("value"), BaseFunction)
-            return RTResult().success(Number.true if is_number else Number.false)
-        execute_is_function.arg_names = ["value"]
-
-        def execute_append(self, exec_ctx):
-            list_ = exec_ctx.symbol_table.get("list")
-            value = exec_ctx.symbol_table.get("value")
-
-            if not isinstance(list_, List):
-            return RTResult().failure(RTError(
-                self.pos_start, self.pos_end,
-                "First argument must be list",
-                exec_ctx
-            ))
-
-            list_.elements.append(value)
-            return RTResult().success(Number.null)
-        execute_append.arg_names = ["list", "value"]
-
-        def execute_pop(self, exec_ctx):
-            list_ = exec_ctx.symbol_table.get("list")
-            index = exec_ctx.symbol_table.get("index")
-
-            if not isinstance(list_, List):
-            return RTResult().failure(RTError(
-                self.pos_start, self.pos_end,
-                "First argument must be list",
-                exec_ctx
-            ))
-
-            if not isinstance(index, Number):
-            return RTResult().failure(RTError(
-                self.pos_start, self.pos_end,
-                "Second argument must be number",
-                exec_ctx
-            ))
-
-            try:
-            element = list_.elements.pop(index.value)
-            except:
-            return RTResult().failure(RTError(
-                self.pos_start, self.pos_end,
-                'Element at this index could not be removed from list because index is out of bounds',
-                exec_ctx
-            ))
-            return RTResult().success(element)
-        execute_pop.arg_names = ["list", "index"]
-
-        def execute_extend(self, exec_ctx):
-            listA = exec_ctx.symbol_table.get("listA")
-            listB = exec_ctx.symbol_table.get("listB")
-
-            if not isinstance(listA, List):
-            return RTResult().failure(RTError(
-                self.pos_start, self.pos_end,
-                "First argument must be list",
-                exec_ctx
-            ))
-
-            if not isinstance(listB, List):
-            return RTResult().failure(RTError(
-                self.pos_start, self.pos_end,
-                "Second argument must be list",
-                exec_ctx
-            ))
-
-            listA.elements.extend(listB.elements)
-            return RTResult().success(Number.null)
-        execute_extend.arg_names = ["listA", "listB"]
-
-BuiltInFunction.print       = BuiltInFunction("print")
-BuiltInFunction.print_ret   = BuiltInFunction("print_ret")
-BuiltInFunction.input       = BuiltInFunction("input")
-BuiltInFunction.input_int   = BuiltInFunction("input_int")
-BuiltInFunction.clear       = BuiltInFunction("clear")
-BuiltInFunction.is_number   = BuiltInFunction("is_number")
-BuiltInFunction.is_string   = BuiltInFunction("is_string")
-BuiltInFunction.is_list     = BuiltInFunction("is_list")
-BuiltInFunction.is_function = BuiltInFunction("is_function")
-BuiltInFunction.append      = BuiltInFunction("append")
-BuiltInFunction.pop         = BuiltInFunction("pop")
-BuiltInFunction.extend      = BuiltInFunction("extend")
 
 class Number(Value):
     """Class for defining arithmetic logic."""
