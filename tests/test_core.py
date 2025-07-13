8b3d65fffda53d311603942554acde55704b40ce
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-07-13 15:30:36 -0400

    tests are indeed still busted

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 # tests/test_core.py
 
-from mariachi.mariachi import run, SymbolTable, Context, Number, BlockNode, IfNode
+from mariachi.mariachi import run, SymbolTable, Context, Number, String, List
 import math
 
 def run_mariachi(code, symbol_table):
@@ -11,63 +11,63 @@ def run_mariachi(code, symbol_table):
     return value
 
 def test_addition(fresh_table):
-    assert run_mariachi("1 + 2", fresh_table) == 3
+    assert run_mariachi("1 + 2", fresh_table) == Number(3)
 
 def test_subtraction(fresh_table):
-    assert run_mariachi("5 - 3", fresh_table) == 2
+    assert run_mariachi("5 - 3", fresh_table) == Number(2)
 
 def test_multiplication(fresh_table):
-    assert run_mariachi("4 * 2", fresh_table) == 8
+    assert run_mariachi("4 * 2", fresh_table) == Number(8)
 
 def test_division(fresh_table):
-    assert run_mariachi("10 / 2", fresh_table) == 5
+    assert run_mariachi("10 / 2", fresh_table) == Number(5)
 
 def test_power(fresh_table):
-    assert run_mariachi("2 ** 3", fresh_table) == 8
+    assert run_mariachi("2 ** 3", fresh_table) == Number(8)
 
 def test_modulo(fresh_table):
-    assert run_mariachi("10 % 3", fresh_table) == 1
+    assert run_mariachi("10 % 3", fresh_table) == Number(1)
 
 def test_floordiv(fresh_table):
-    assert run_mariachi("10 // 3", fresh_table) == 3
+    assert run_mariachi("10 // 3", fresh_table) == Number(3)
 
 def test_equals(fresh_table):
-    assert run_mariachi("2 == 2", fresh_table) == 1
-    assert run_mariachi("2 == 3", fresh_table) == 0
+    assert run_mariachi("2 == 2", fresh_table) == Number(1)
+    assert run_mariachi("2 == 3", fresh_table) == Number(0)
 
 def test_not_equals(fresh_table):
-    assert run_mariachi("2 != 3", fresh_table) == 1
-    assert run_mariachi("2 != 2", fresh_table) == 0
+    assert run_mariachi("2 != 3", fresh_table) == Number(1)
+    assert run_mariachi("2 != 2", fresh_table) == Number(0)
 
 def test_comparisons(fresh_table):
-    assert run_mariachi("2 < 3", fresh_table) == 1
-    assert run_mariachi("3 <= 3", fresh_table) == 1
-    assert run_mariachi("4 > 2", fresh_table) == 1
-    assert run_mariachi("4 >= 5", fresh_table) == 0
+    assert run_mariachi("2 < 3", fresh_table) == Number(1)
+    assert run_mariachi("3 <= 3", fresh_table) == Number(1)
+    assert run_mariachi("4 > 2", fresh_table) == Number(1)
+    assert run_mariachi("4 >= 5", fresh_table) == Number(0)
 
 def test_logical_ops(fresh_table):
-    assert run_mariachi("1 y 0", fresh_table) == 0
-    assert run_mariachi("1 o 0", fresh_table) == 1
-    assert run_mariachi("jamas 0", fresh_table) == 1
-    assert run_mariachi("jamas 1", fresh_table) == 0
+    assert run_mariachi("1 y 0", fresh_table) == Number(0)
+    assert run_mariachi("1 o 0", fresh_table) == Number(1)
+    assert run_mariachi("jamas 0", fresh_table) == Number(1)
+    assert run_mariachi("jamas 1", fresh_table) == Number(0)
 
 def test_variable_assign(fresh_table):
     run_mariachi("sea x = 7", fresh_table)
-    assert run_mariachi("x + 3", fresh_table) == 10
+    assert run_mariachi("x + 3", fresh_table) == Number(10)
 
 def test_variable_access(fresh_table):
     run_mariachi("sea x = 7", fresh_table)
-    assert run_mariachi("x", fresh_table) == 7
+    assert run_mariachi("x", fresh_table) == Number(7)
 
 def test_loop_for(fresh_table):
     run('programma',"sea r = 1", fresh_table)
-    run('programma',"para i = 1 hasta 6 pues sea r = r * i", fresh_table)
-    assert run_mariachi("r", fresh_table) == 120
+    run('programma',"para i = 1 hasta 6 { sea r = r * i }", fresh_table)
+    assert run_mariachi("r", fresh_table) == Number(120)
 
 def test_if(fresh_table):
     run('programma',"sea r = 1", fresh_table)
     run('programma', 'si r == 1 pues sea r = r + 1', fresh_table)
-    assert run_mariachi("r", fresh_table) == 2
+    assert run_mariachi("r", fresh_table) == Number(2)
 
 def test_constants(fresh_table):
     run_mariachi("fija PI = 3.14", fresh_table)
@@ -89,8 +89,8 @@ def test_const_overwrite():
 
 def test_function_declaration(fresh_table):
     run('programma', "define hola(x) { x + 1 }", fresh_table)
-    result, error = run('programma', "hola(2)", fresh_table)
-    assert result.value == 3
+    result = run('programma', "hola(2)", fresh_table)
+    assert result.value == Number(3)
 
 def test_string(fresh_table):
     result = run_mariachi('"this is a string"', fresh_table)
@@ -99,23 +99,24 @@ def test_string(fresh_table):
     assert result == 'this is a string'
     assert result_2 == 'this is a string'
 
-#def test_list_manipulation(fresh_table): ---> not possible yet as I need to figure out how to test a class instance version of our list
-#    result, error = run('programma', '[1,2,3,4] * [1,2,3]', fresh_table)
-#    result_2, error = run('programma', '[1,2,3,4] / 0', fresh_table)
-#    result_3, error = run('programma', '[1,2,3] + 4', fresh_table)
-#    assert result.elements == [1, 2, 3, 4, 1, 2, 3]
-#    assert result_2.elements == 1
-#    assert result_3.elements == [1, 2, 3, 4]
+def test_list_manipulation(fresh_table):
+    result = run('programma', '[1,2,3,4] * [1,2,3]', fresh_table)
+    result_2 = run('programma', '[1,2,3,4] / 0', fresh_table)
+    result_3 = run('programma', '[1,2,3] + 4', fresh_table)
+    assert result.elements == List([1, 2, 3, 4, 1, 2, 3])
+    assert result_2.elements == List(1)
+    assert result_3.elements == List([1, 2, 3, 4])
 
 def test_builtins(fresh_table):
     #assert run_mariachi('canta("x")', fresh_table) == 'x' print returns 0 for some reason
-    assert run_mariachi('eco("x")', fresh_table) == 'x'
+    result = run_mariachi('eco("x")', fresh_table)
+    print(type(result))
+    assert result == String('x')
 
 def test_if_else(fresh_table):
-    # Create a mock IfNode with else_case
-    result = run_mariachi("si 2 == 1 { canta(1) } sino { canta(0) }", fresh_table)
-    print(result)
+    result = run_mariachi("si 2 == 1 { eco(1) } sino { eco(0) }", fresh_table)
+    assert result == Number(0)
 
 def test_if(fresh_table):
-    result = run_mariachi("si 1 == 1 { canta(1) }", fresh_table)
-    assert result == 0
+    result = run_mariachi("si 1 == 1 { eco(1) }", fresh_table)
+    assert result == 1
